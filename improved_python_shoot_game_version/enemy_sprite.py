2ba6c00293ab5617862cde5fddcffb03bae05554
unknown <achillesaeon8-svg>
2026-06-21 23:13:48 +0800

    Removal of keyword speed and explicitly assign rect bounding box.

--- improved_python_shoot_game_version/enemy_sprite.py
+++ improved_python_shoot_game_version/enemy_sprite.py
@@ -4,9 +4,10 @@ from core_abstract_blueprint import GameObject
 class EnemyShips(GameObject):
     def __init__(self, enemy_image, enemy_down_image, initial_position):
         
-        super().__init__(enemy_image, initial_position, speed=2)
-        if self.rect:
-            self.rect.topleft = initial_position
+        super().__init__(enemy_image, initial_position, 2)
+
+        self.rect = enemy_image.get_rect()
+        self.rect.topleft = initial_position
         self.down_imgs = enemy_down_image
         self.down_index = 0
 
