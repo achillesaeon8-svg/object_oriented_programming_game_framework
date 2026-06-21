8dd212a9a786b1593f489c0e594a0b126963b2e8
unknown <achillesaeon8-svg>
2026-06-21 22:21:33 +0800

    Removal of unnecessary comments and spaces in the main file. Adding Module Imports.

--- improved_python_shoot_game_version/mainGame.py
+++ improved_python_shoot_game_version/mainGame.py
@@ -1,25 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 11 11:05:00 2013
-
-@author: Leo
-"""
-
 import pygame
 from sys import exit
 from pygame.locals import *
-from improved_python_shoot_game_version.player_plane import *
 import random
 import os
+from global_configuration_constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player_plane import Player
+from enemy_sprite import Enemy
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# 初始化游戏
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('飞机大战')
 
-# 载入游戏音乐
 bullet_sound = pygame.mixer.Sound(os.path.join(BASE_DIR, 'resources', 'sound', 'bullet.wav'))
 enemy1_down_sound = pygame.mixer.Sound(os.path.join(BASE_DIR, 'resources', 'sound', 'enemy1_down.wav'))
 game_over_sound = pygame.mixer.Sound(os.path.join(BASE_DIR, 'resources', 'sound', 'game_over.wav'))
@@ -30,18 +23,17 @@ pygame.mixer.music.load(os.path.join(BASE_DIR, 'resources', 'sound', 'game_music
 pygame.mixer.music.play(-1, 0.0)
 pygame.mixer.music.set_volume(0.25)
 
-# 载入背景图
 background = pygame.image.load(os.path.join(BASE_DIR, 'resources', 'image', 'background.png')).convert()
 game_over = pygame.image.load(os.path.join(BASE_DIR, 'resources', 'image', 'gameover.png'))
 
 filename = pygame.image.load(os.path.join(BASE_DIR, 'resources', 'image', 'shoot.png'))
 plane_img = pygame.image.load(os.path.join(BASE_DIR, 'resources', 'image', 'shoot.png'))
 
-# 设置玩家相关参数
+
 player_rect = []
-player_rect.append(pygame.Rect(0, 99, 102, 126))        # 玩家精灵图片区域
+player_rect.append(pygame.Rect(0, 99, 102, 126))
 player_rect.append(pygame.Rect(165, 360, 102, 126))
-player_rect.append(pygame.Rect(165, 234, 102, 126))     # 玩家爆炸精灵图片区域
+player_rect.append(pygame.Rect(165, 234, 102, 126))
 player_rect.append(pygame.Rect(330, 624, 102, 126))
 player_rect.append(pygame.Rect(330, 498, 102, 126))
 player_rect.append(pygame.Rect(432, 624, 102, 126))
@@ -78,10 +70,8 @@ clock = pygame.time.Clock()
 running = True
 
 while running:
-    # 控制游戏最大帧率为60
     clock.tick(45)
 
-    # 控制发射子弹频率,并发射子弹
     if not player.is_hit:
         if shoot_frequency % 15 == 0:
             bullet_sound.play()
@@ -99,16 +89,13 @@ while running:
     if enemy_frequency >= 100:
         enemy_frequency = 0
 
-    # 移动子弹，若超出窗口范围则删除
     for bullet in player.bullets:
         bullet.move()
         if bullet.rect.bottom < 0:
             player.bullets.remove(bullet)
 
-    # 移动敌机，若超出窗口范围则删除
     for enemy in enemies1:
         enemy.move()
-        # 判断玩家是否被击中
         if pygame.sprite.collide_circle(enemy, player):
             enemies_down.add(enemy)
             enemies1.remove(enemy)
@@ -118,19 +105,16 @@ while running:
         if enemy.rect.top > SCREEN_HEIGHT:
             enemies1.remove(enemy)
 
-    # 将被击中的敌机对象添加到击毁敌机Group中，用来渲染击毁动画
     enemies1_down = pygame.sprite.groupcollide(enemies1, player.bullets, 1, 1)
     for enemy_down in enemies1_down:
         enemies_down.add(enemy_down)
 
-    # 绘制背景
     screen.fill(0)
     screen.blit(background, (0, 0))
 
-    # 绘制玩家飞机
     if not player.is_hit:
         screen.blit(player.image[player.img_index], player.rect)
-        # 更换图片索引使飞机有动画效果
+
         player.img_index = shoot_frequency // 8
     else:
         player.img_index = player_down_index // 8
@@ -139,7 +123,7 @@ while running:
         if player_down_index > 47:
             running = False
 
-    # 绘制击毁动画
+
     for enemy_down in enemies_down:
         if enemy_down.down_index == 0:
             enemy1_down_sound.play()
@@ -150,18 +134,16 @@ while running:
         screen.blit(enemy_down.down_imgs[enemy_down.down_index // 2], enemy_down.rect)
         enemy_down.down_index += 1
 
-    # 绘制子弹和敌机
     player.bullets.draw(screen)
     enemies1.draw(screen)
 
-    # 绘制得分
     score_font = pygame.font.Font(None, 36)
     score_text = score_font.render(str(score), True, (128, 128, 128))
     text_rect = score_text.get_rect()
     text_rect.topleft = [10, 10]
     screen.blit(score_text, text_rect)
 
-    # 更新屏幕
+
     pygame.display.update()
 
     for event in pygame.event.get():
@@ -169,9 +151,8 @@ while running:
             pygame.quit()
             exit()
             
-    # 监听键盘事件
     key_pressed = pygame.key.get_pressed()
-    # 若玩家被击中，则无效
+
     if not player.is_hit:
         if key_pressed[K_w] or key_pressed[K_UP]:
             player.moveUp()
