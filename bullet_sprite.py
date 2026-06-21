7063abd1dc4f5ab64dcafa505d141621a3c37a65
unknown <achillesaeon8-svg>
2026-06-21 21:44:33 +0800

    Constructor Initilization and Applying Inheritance of due to Parent Delegation within bullet Class File.

--- bullet_sprite.py
+++ bullet_sprite.py
@@ -3,11 +3,8 @@ from core_abstract_blueprint import GameObject
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, bullet_img, init_pos):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = bullet_img
-        self.rect = self.image.get_rect()
-        self.rect.midbottom = init_pos
-        self.speed = 10
+        
+        super().__init__(bullet_img, init_pos, speed=10)
 
     def move(self):
         self.rect.top -= self.speed
