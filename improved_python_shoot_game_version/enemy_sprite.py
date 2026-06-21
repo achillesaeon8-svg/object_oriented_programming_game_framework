ee93b714a057fb926a488aa7a1fd81ec09d7a30f
unknown <achillesaeon8-svg>
2026-06-21 21:59:50 +0800

    Adding Instance tracking state to manage death animations when shot.

--- improved_python_shoot_game_version/enemy_sprite.py
+++ improved_python_shoot_game_version/enemy_sprite.py
@@ -3,7 +3,11 @@ from core_abstract_blueprint import GameObject
 
 class Enemy(GameObject):
     def __init__(self, enemy_img, enemy_down_imgs, init_pos):
-       super().__init__(enemy_img, init_pos, speed=2)
-
+        super().__init__(enemy_img, init_pos, speed=2)
+        if self.rect:
+            self.rect.topleft = init_pos
+        self.down_imgs = enemy_down_imgs
+        self.down_index = 0
+        
     def move(self):
         self.rect.top += self.speed
